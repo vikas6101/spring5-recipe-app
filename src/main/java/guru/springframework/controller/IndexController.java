6a788b1749484d254f6143c8b5637688ee234d5b
Vikas Singh <askforvikas@gmail.com>
2021-08-07 01:09:29 +0400

    Initial commit JPA entity

---
+++ src/main/java/guru/springframework/controller/IndexController.java
@@ -0,0 +1,7 @@
+package guru.springframework.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+}
---
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -0,0 +1,41 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNote;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNote() {
+        return recipeNote;
+    }
+
+    public void setRecipeNote(String recipeNote) {
+        this.recipeNote = recipeNote;
+    }
+}
